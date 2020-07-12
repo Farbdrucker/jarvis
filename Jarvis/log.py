94301a918f11da9f8eabc4e7c0423ba7f775e12f
Farbdrucker <l.sanner@gmx.de>
2020-07-12 19:04:09 +0200

    mv color up

--- Jarvis/log.py
+++ Jarvis/log.py
@@ -1,6 +1,19 @@
 from Jarvis.util import now
 
 
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
 class logging:
     @staticmethod
     def prep(func):
@@ -33,16 +46,3 @@ class logging:
     @staticmethod
     def debug(args):
         logging.pprint(logging.prep("debug") + args)
-
-
-class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
